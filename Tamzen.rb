89c960ab731fd64703e2bece75b19a7cd4616deb
Suraj N. Kurapati <sunaku@gmail.com>
2012-10-07 13:42:37 -0700

    move specification away from implementation details

--- Tamzen.rb
+++ Tamzen.rb
@@ -1,3 +1,22 @@
+#-----------------------------------------------------------------------------
+# specification
+#-----------------------------------------------------------------------------
+
+BACKPORTS = [
+  # backport the given CHARACTER at the given WEIGHT from the given VERSIONS
+  # (try them all, one by one, until one of them has the specified character)
+  {character: 'g', weight: /Medium/, versions: ['v1.6']},
+  {character: 'h', weight: /Medium/, versions: ['v1.6']},
+  {character: 'm', weight: /Medium/, versions: ['v1.6']},
+  {character: 'l', weight: //,       versions: ['v1.6']},
+  {character: 'w', weight: //,       versions: ['v1.6']},
+  {character: 'y', weight: //,       versions: ['v1.6']},
+]
+
+#-----------------------------------------------------------------------------
+# implementation
+#-----------------------------------------------------------------------------
+
 class Font < Struct.new(:file, :props, :chars)
   def initialize blob
     head, *body, @tail = blob.data.split(/(?=\nSTARTCHAR|\nENDFONT)/)
@@ -9,7 +28,7 @@ class Font < Struct.new(:file, :props, :chars)
   def to_s
     props['CHARS'] = chars.length
     [
-      props.map {|*a| a.join(' ') }.join("\n"), "\n",
+      props.map {|*a| a.join(' ') }.join(?\n), ?\n,
       chars.values,
       @tail
     ].join
@@ -17,37 +36,31 @@ class Font < Struct.new(:file, :props, :chars)
 end
 
 require 'grit'
-GIT = Grit::Repo.new('.')
-GIT.tree('v1.9').blobs.each do |blob|
-  if blob.name =~ /\.bdf$/
+repo = Grit::Repo.new('.')
+trees = Hash.new {|h,k| h[k] = repo.tree(k) }
+trees['v1.9'].blobs.each do |blob|
+  if blob.name.end_with? '.bdf'
     begin
       font = Font.new(blob)
 
-      # delete empty glyphs except space and non-breaking space
+      # delete empty glyphs except space (32) and non-breaking space (160)
       font.chars.each do |code, char|
-        if code != 32 && code != 160 && char =~ /BITMAP\n(?:00\n)+ENDCHAR/
+        if char =~ /BITMAP\n(?:00\n)+ENDCHAR/ && code != 32 && code != 160
           font.chars.delete code
         end
       end
 
       # backport characters from older versions of the font
-      [
-        ['g'.ord, /Medium/, 'v1.6'],
-        ['h'.ord, /Medium/, 'v1.6'],
-        ['m'.ord, /Medium/, 'v1.6'],
-        ['l'.ord, //,       'v1.6'],
-        ['w'.ord, //,       'v1.6'],
-        ['y'.ord, //,       'v1.6'],
-      ].
-      each do |code, weight, *versions|
-        if font.props['WEIGHT_NAME'] =~ weight
-          versions.any? do |version|
-            if old_blob = GIT.tree(version) / font.file
+      BACKPORTS.each do |backport|
+        code = backport[:character].ord
+        if font.props['WEIGHT_NAME'] =~ backport[:weight]
+          backport[:versions].any? do |version|
+            if old_blob = trees[version] / font.file
               old_font = Font.new(old_blob)
               font.chars[code] = old_font.chars[code]
               true
             end
-          end or raise "versions #{versions} lack #{font.file}"
+          end or raise "versions #{backport[:versions]} lack #{font.file}"
         end
       end
 
